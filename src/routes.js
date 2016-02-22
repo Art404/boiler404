5b331759303b3849ed65e7598aab299292bc27e9
manny404 <manny@art404.com>
2016-02-21 22:31:10 -0500

    history

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import {Route, IndexRoute} from 'react-router'
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 import App from './components/App/App'
 import Page from './components/Page/Page'
 import About from './components/About/About'
 import ProjectDetail from './components/ProjectDetail/ProjectDetail'
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={Page}/>
-    <Route name="project" path="about" component={About}/>
-    <Route name="project" path="project/:projectID" component={ProjectDetail}/>
-    <Route name="category" path="/:cat" component={Page}>
-      <Route path="tagged/:subtag" component={Page} />
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Page}/>
+      <Route name="project" path="about" component={About}/>
+      <Route name="project" path="project/:projectID" component={ProjectDetail}/>
+      <Route name="category" path="/:cat" component={Page}>
+        <Route path="tagged/:subtag" component={Page} />
+      </Route>
     </Route>
-  </Route>
+  </Router>
 )
